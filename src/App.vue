107df4aba5c5221d4f2ce21043260e61e16be6e1
Ramir <kolesnikramir@gmail.com>
2023-03-18 11:47:33 +0500

    created buttons.scss

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
+      <div class="double__btn">
+        <a href="" class="btn white center">Hello</a>
+        <a href="" class="btn black center">World</a>
+      </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
 </style>
